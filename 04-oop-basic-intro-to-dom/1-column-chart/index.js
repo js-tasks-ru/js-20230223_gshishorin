c9c18e0573e6477467a163687cf4ca443be30663
George Shishorin <george.shishorin@exness.com>
2023-03-15 15:48:17 +0200

    initial

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,93 @@
 export default class ColumnChart {
+  chartHeight = 50;
 
+  constructor({
+    data = [],
+    label = "",
+    value = 0,
+    link = "",
+    formatHeading = (data) => data,
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.value = formatHeading(value);
+    this.link = link;
+  }
+
+  get template() {
+    return `
+      <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+            Total ${this.label}
+            ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${
+            this.value
+          }</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnElements()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  get element() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.template;
+    const _element = wrapper.firstElementChild;
+    if (!this.data.length) {
+      _element.classList.add("column-chart_loading");
+    }
+    return _element;
+  }
+
+  getLink() {
+    return this.link
+      ? `<a class="column-chart__link" href="${this.link}">View all</a>`
+      : "";
+  }
+
+  getColumnElements() {
+    if (this.data.length > 0) {
+      const columnBody = [];
+      const columnProps = this.getColumnProps(this.data);
+      for (let prop of columnProps) {
+        columnBody.push(
+          `<div style="--value: ${prop.value}" data-tooltip="${prop.percent}"></div>`
+        );
+      }
+      columnBody.join("");
+      return columnBody;
+    }
+  }
+
+  getColumnProps(data) {
+    const maxValue = Math.max(...data);
+    const scale = 50 / maxValue;
+
+    return data.map((item) => {
+      return {
+        percent: ((item / maxValue) * 100).toFixed(0) + "%",
+        value: String(Math.floor(item * scale)),
+      };
+    });
+  }
+
+  update(data) {
+    if (this.data.length) {
+      this.data = data;
+    }
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
